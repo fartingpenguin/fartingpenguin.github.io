7b06221947bf847615fd3b11dbcf1027ba65bf29
justsumenglishstuff <98941473+justsumenglishstuff@users.noreply.github.com>
2022-02-03 16:19:14 +0800

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -7,14 +7,3 @@ wrapper.addEventListener('mousemove', (event) => {
     background.style.marginLeft = moveX + 'px';
     background.style.marginTop = moveY + 'px';
 });
-
-// Particles
-particlesJS.load('particles', 'assets/particles.json');
-
-// Minecraft players status
-MinecraftAPI.getServerStatus(document.getElementById('server-ip').innerHTML, { port: 25565 }, (error, status) => {
-    if (error) {
-        return document.getElementById('server-status').innerHTML = 'Error Loading Status!';
-    }
-    document.getElementById('server-players').innerHTML = status.players.now;
-});
